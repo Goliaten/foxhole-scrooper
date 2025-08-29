b632ffee84a63376d4c703ffc7206bd62cae49b2
Goliaten <ognikat4@gmail.com>
2025-08-29 20:04:33 +0200

    MSSScreenshotTaker implemented

--- source/modules/screenshot_takers/BaseScreenshotTaker.py
+++ source/modules/screenshot_takers/BaseScreenshotTaker.py
@@ -1,9 +1,8 @@
 from abc import abstractmethod
-from PIL import Image
+from PIL.Image import Image
 from source.modules.BaseModule import BaseModule
 
 
 class BaseScreenshotTaker(BaseModule):
     @abstractmethod
-    def take_screenshot(self) -> Image.Image:
-        raise NotImplementedError
+    def take_screenshot(self) -> Image: ...
--- source/modules/screenshot_takers/MSSScreenshotTaker.py
+++ source/modules/screenshot_takers/MSSScreenshotTaker.py
@@ -1,6 +1,26 @@
 from source.modules.screenshot_takers.BaseScreenshotTaker import BaseScreenshotTaker
+import mss
+
+# from PIL.Image import Image
+from PIL import Image
 
 
 class MSSScreenshotTaker(BaseScreenshotTaker):
-    def take_screenshot(self):
-        return super().take_screenshot()
+    def take_screenshot(self) -> Image.Image:
+        with mss.mss() as sct:
+            try:
+                mon = sct.monitors[
+                    int(self.config.get("ScreenshotTaker", {}).get("monitor_number", 1))
+                ]
+            except IndexError:
+                print(
+                    f"Invalid monitor index (`monitor_number` parameter). Allowed range is <0,{len(sct.monitors) - 1}>"
+                )
+                exit(1)
+            sct_img = sct.grab(mon)
+
+            img = Image.new("RGB", sct_img.size)
+            pixels = zip(sct_img.raw[2::4], sct_img.raw[1::4], sct_img.raw[::4])
+            img.putdata(list(pixels))
+
+            return img
