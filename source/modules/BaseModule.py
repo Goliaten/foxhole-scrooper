126394a27c7094d63c68be24448416b9eb6ca7c4
Goliaten <ognikat4@gmail.com>
2025-08-31 12:27:52 +0200

    move params loading into helper function

--- source/modules/BaseModule.py
+++ source/modules/BaseModule.py
@@ -1,17 +1,11 @@
 from typing import Any, Dict
-import toml
 
+from source.helpers.Params import read_params
 from source.helpers.Singleton import Singleton
-import source.config as cfg
 
 
 class BaseModule(metaclass=Singleton):
     config: Dict[str, Any]
 
     def __init__(self) -> None:
-        self.config = self.read_params()
-
-    @staticmethod
-    def read_params() -> Dict[str, Any]:
-        with open(cfg.PARAMS_PATH, "r") as file:
-            return toml.load(file)
+        self.config = read_params()
