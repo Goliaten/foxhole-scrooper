a7297fc48144fdcc6efcef8113ba6a7882a7f495
Goliaten <ognikat4@gmail.com>
2025-08-31 12:46:27 +0200

    update to base data extractor

--- source/modules/DataExtractors/BaseDataExtractor.py
+++ source/modules/DataExtractors/BaseDataExtractor.py
@@ -1,5 +1,8 @@
+from abc import abstractmethod
+from typing import Any
 from source.modules.BaseModule import BaseModule
 
 
 class BaseDataExtractor(BaseModule):
-    pass
+    @abstractmethod
+    def process_data(self, data: Any) -> Any: ...
