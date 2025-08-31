ac6b09583d21f38990f51655d57868bcb0355025
Goliaten <ognikat4@gmail.com>
2025-08-31 12:28:20 +0200

    modified offset helper

--- source/helpers/OffsetPoint.py
+++ source/helpers/OffsetPoint.py
@@ -1,8 +1,9 @@
 from typing import Any, Dict, Tuple
+from source.helpers.Params import read_params
 
 
 def offset_point(
-    self, pos: Tuple[int, int], offset: Tuple[int, int], reverse=False
+    pos: Tuple[int, int], offset: Tuple[int, int], reverse=False
 ) -> Tuple[int, int]:
     """
     Offsets the `pos` tuple by `offset` tuple.
@@ -22,7 +23,7 @@ def offset_point(
 
 
 def offset_point_provided_config(
-    self, pos: Tuple[int, int], config: Dict[str, Any], reverse=False
+    pos: Tuple[int, int], config: Dict[str, Any], reverse=False
 ) -> Tuple[int, int]:
     """
     Offsets the `pos` tuple by offset_x and offset_y from "MovementController" key from parameters.
@@ -36,6 +37,21 @@ def offset_point_provided_config(
     """
     off_x = config.get("MovementController", {}).get("offset_x", 0)
     off_y = config.get("MovementController", {}).get("offset_y", 0)
-    if reverse:
-        return pos[0] - off_x, pos[1] - off_y
-    return pos[0] + off_x, pos[1] + off_y
+    return offset_point(pos=pos, offset=(off_x, off_y), reverse=reverse)
+
+
+def offset_point_read_config(pos: Tuple[int, int], reverse=False) -> Tuple[int, int]:
+    """
+    Offsets the `pos` tuple by offset_x and offset_y from "MovementController" key from parameters.
+
+    `pos` represents coordinates on 2d plane.
+    1st index represents X value.
+    2nd index represents Y value.
+
+    If `reverse` is False (default), then offsets are added to `pos`.
+    Otherwise offsets is substracted from `pos`.
+    """
+    config = read_params()
+    off_x = config.get("MovementController", {}).get("offset_x", 0)
+    off_y = config.get("MovementController", {}).get("offset_y", 0)
+    return offset_point(pos=pos, offset=(off_x, off_y), reverse=reverse)
