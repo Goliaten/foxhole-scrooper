026deb50912356e92489cc18c1e5d6063985c6a5
Goliaten <ognikat4@gmail.com>
2025-09-14 14:01:25 +0200

    test run

--- source/core.py
+++ source/core.py
@@ -1,6 +1,7 @@
 from source.modules.DataExtractors.AOIExtractor import AOIExtractor
 from source.modules.DataProcessors.Scrooper import Scrooper
 from source.modules.ImageAcquirers.MSSImageAcquirer import MSSImageAcquirer
+from source.modules.MovementController.PyAutoGui import PyAutoGui
 
 
 class Core:
@@ -37,39 +38,14 @@ class Core:
                 return 0
             return sum(lst) / len(lst)
 
-        times_100 = []
-        times_100_img = []
-        times_100_aoi = []
-        times_100_scrooper = []
+        PyAutoGui()
         while True:
             time.sleep(1)
-            t0 = time.time_ns()
+            print("-------")
             img = MSSImageAcquirer().take_screenshot()
-            t1a = time.time_ns()
             event = AOIExtractor().extract_data_from_pil_image(img)
-            t1b = time.time_ns()
-            Scrooper().process_data(event)
-            t1 = time.time_ns()
-
-            times_100.append(t1 - t0)
-            times_100_img.append(t1a - t0)
-            times_100_aoi.append(t1b - t1a)
-            times_100_scrooper.append(t1 - t1b)
-
-            while len(times_100_img) > 100:
-                times_100_img.pop(0)
-            while len(times_100_aoi) > 100:
-                times_100_aoi.pop(0)
-            while len(times_100_scrooper) > 100:
-                times_100_scrooper.pop(0)
-            while len(times_100) > 100:
-                times_100.pop(0)
-            print(
-                f"Total: {round(avg(times_100) / 1e9, 6)}s; "
-                f"Image: {round(avg(times_100_img) / 1e9, 6)}s; "
-                f"AOI: {round(avg(times_100_aoi) / 1e9, 6)}s; "
-                f"Scrooper: {round(avg(times_100_scrooper) / 1e9, 6)}s"
-            )
+            event = Scrooper().process_data(event)
+            PyAutoGui().act_on_event(event)
         exit()
 
         while True:
