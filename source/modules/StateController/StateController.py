b4a1ab1547eff01011d3dbb5c2709d36e30ffea0
Goliaten <ognikat4@gmail.com>
2025-09-16 23:30:43 +0200

    TODO for state controller

---
+++ source/modules/StateController/StateController.py
@@ -0,0 +1,11 @@
+# TODO:
+# - make controller which holds at least following states
+#   - scrooping
+#   - looking for salvage - it should be lifted if either no salvage is found, or we're above salvage, and ready to gather it
+#   - hammering salvage - it should only be lifted if we've confirmed, that no salvage is being gathered or we've got full inventory
+#   - moving
+#   - inventory state - not full, full
+#   - encumberance - none, close_full, full
+#   - got salvage recently - check with ocr if `gathering salvage` message was shown recently. timeout of ~3 seconds??
+#   - dead - if character is dead
+
